cd8f1912ab9591bbc57a37331201947bc8b4bc42
Sanchit Rohilla <team@gmail.com>
2026-08-07 10:01:36 +0000

    refactor: extract artist dashboard analytics section

--- resources/js/V2/Shared/Dashboard/Sections/index.js
+++ resources/js/V2/Shared/Dashboard/Sections/index.js
@@ -1,2 +1,3 @@
 export { default as DashboardHero } from './DashboardHero';
 export { default as DashboardKpiGrid } from './DashboardKpiGrid';
+export { default as DashboardAnalyticsSection } from './DashboardAnalyticsSection';
